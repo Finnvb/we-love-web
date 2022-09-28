c413b9bd6ef97d1d3d9ceb7dde879e935f4a2f38
Finnvb <finnvb@gmail.com>
2022-09-28 11:28:17 +0200

    .

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,29 @@
+<script>
+
+  export let name;
+</script>
+
+
+<header>
+    <h1>{name}.</h1>
+  
+  
+  
+      
+  </header>
+  
+ 
+  
+  
+  <style>
+    header {
+      display: flex;
+      justify-content: space-between;
+      color: #ffffff;
+      font-weight: bold;
+      font-size: 1.5rem;
+      padding: 1rem;
+  background-color: black;
+    }
+  
+  </style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Header from '$lib/components/Header.svelte'
+
+    import '../styles/global.css'
+</script>
+
+
+<Header name={'Finn van Bekkum'}/>
+
+
+
+<slot></slot>
